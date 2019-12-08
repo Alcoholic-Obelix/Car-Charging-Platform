11d97738617f9a7ce7e7d45121e15911db7429c8
Ricardo Reis <ricardo_reis@live.com>
2019-12-08 16:39:44 +0000

    *ModelUpdated

--- pweb1920/pweb1920/DAL/Company.cs
+++ pweb1920/pweb1920/DAL/Company.cs
@@ -11,9 +11,7 @@ namespace pweb1920.DAL
 {
     using System;
     using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations.Schema;
-    using System.Web.Mvc;
-
+    
     public partial class Company
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -21,17 +19,14 @@ namespace pweb1920.DAL
         {
             this.Station = new HashSet<Station>();
         }
-
+    
         public int Id { get; set; }
         public string Name { get; set; }
         public string NIF { get; set; }
         public string Status { get; set; }
         public string IdentityId { get; set; }
-
+    
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Station> Station { get; set; }
-
-        [NotMapped]
-        public List<SelectListItem> StatusDropDown { get; set; }
     }
 }
